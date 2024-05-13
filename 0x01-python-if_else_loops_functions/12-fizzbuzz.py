bb4fd01ca9fb7a45f0106de6d4a7c7eca8f57841
ddanimoh <ddanimoh@gmail.com>
2024-05-13 11:05:36 +0100

    fizzBuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+
+    for x in range(1, 101):
+        if x % 3 == 0 and x % 5 == 0:
+            print("{:} ".format('FizzBuzz'), end='')
+        elif x % 5 == 0:
+            print("{:} ".format('Buzz'), end='')
+        elif x % 3 == 0:
+            print("{:} ".format('Fizz'), end='')
+        else:
+            print("{:} ".format(x), end='')
+    return
