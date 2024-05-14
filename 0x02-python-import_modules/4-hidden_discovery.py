d8e3c1828254fb2d01ca5e68ef5108c2ab521a77
ddanimoh <ddanimoh@gmail.com>
2024-05-14 05:46:33 +0100

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
 
     hidden = dir(hidden_4)
 
-    for i in range(len(hidden)):
+    for x in range(len(hidden)):
         if hidden[x][0] != '_' and hidden[x][1] != '_':
             print(hidden[x])
