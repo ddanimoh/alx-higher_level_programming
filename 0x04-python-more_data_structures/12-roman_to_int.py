4138c8a82a8cba216f3de0590bd7ce8268dd9738
ddanimoh <ddanimoh@gmail.com>
2024-05-16 17:17:00 +0100

    Romance to Decimal

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rome = 0
+    prev_value = 0
+
+    for char in roman_string:
+        current_value = dic[char]
+        if prev_value < current_value:
+            rome += current_value - 2 * prev_value
+        else:
+            rome += current_value
+        prev_value = current_value
+    return rome
