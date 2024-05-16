56f3de7cb2987fb67bdecc55cd87aa6f31fd1659
ddanimoh <ddanimoh@gmail.com>
2024-05-16 17:23:44 +0100

    Romance to Decimal

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,6 +6,9 @@ def roman_to_int(roman_string):
     rome = 0
     prev_value = 0
 
+    if roman_string is None or roman_string[0] <= 9:
+        return 0
+
     for char in roman_string:
         current_value = dic[char]
         if prev_value < current_value:
