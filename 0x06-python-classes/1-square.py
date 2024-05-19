c1086bec606929dff26f561c26e3163d3550bf79
ddanimoh <ddanimoh@gmail.com>
2024-05-19 02:26:09 +0100

    class with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+A defination of Square class
+
+"""
+
+
+class Square:
+    """
+    A class with a private attribute size
+    """
+
+    def __init__(self, size):
+        self.__size = size
