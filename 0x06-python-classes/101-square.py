1c8f3840b17cf2a2f87c024d9d061539a659f0bd
ddanimoh <ddanimoh@gmail.com>
2024-06-12 02:10:27 +0100

    sqr

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,63 +1,58 @@
 #!/usr/bin/python3
-
 '''
-A square class with private instance with Method
+Continue class sqr
 '''
 
 
 class Square:
 
     '''
-    A class with a private attribute size
-    With instantiation of 0, with Error check condition
+    Comment for class comes here explain what the class does
     '''
+
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        return self._size
+        return self.__size
 
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         if value < 0:
-            raise ValueError("size must be >= 0")
-        self._size = value
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
     @property
     def position(self):
-        return self._position
+        return self.__position
 
     @position.setter
     def position(self, value):
-        if not (isinstance(value, tuple) and len(value) == 2 and
-                all(isinstance(num, int) and num >= 0 for num in value)):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(i, int) and i >= 0 for i in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self._position = value
+        self.__position = value
 
     def area(self):
-        return self._size ** 2
+        return self.__size ** 2
 
     def my_print(self):
-        if self._size == 0:
-            print()
-            return
-
-        [print() for _ in range(self._position[1])]
-        for i in range(self._size):
-            print(" " * self._position[0] + "#" * self._size)
+        print(self.__str__())
 
     def __str__(self):
         if self.__size == 0:
             return ""
 
         result = []
+        # Append the leading new lines based on position[1]
         result.append("\n" * self.__position[1])
-        
+
         for _ in range(self.__size):
             result.append(" " * self.__position[0] + "#" * self.__size + "\n")
-        
+
         return "".join(result).rstrip()
