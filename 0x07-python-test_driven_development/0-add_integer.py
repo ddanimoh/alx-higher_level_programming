c6131d1e0ff2517cf14a3a2f67bf951ac50c855b
ddanimoh <ddanimoh@gmail.com>
2024-06-14 07:35:35 +0100

    add function

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+add_integer function adds two integers or floats
+and returns the result as an integer.
+
+The add_integer function takes two arguments, with the second one being optional.
+
+The result or output is always going to be an integer
+even if float numbers are added.
+"""
+def add_integer(a, b=98):
+    
+    """
+    Adds two integers or floats and returns the result as an integer.
+
+    Args:
+        a (int, float): The first number to add. Must be an int or float.
+        b (int, float): The second number to add. Default is 98. Must be an int
+        or float.
+    Returns:
+        int: The sum of a and b, casted to an integer.
+
+    Raises:
+        TypeError: If either a or b is not an integer or float.
+    """
+
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("a and b must be integers or floats")
+
+    return int(a) + int(b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+0x07-python-test_driven_development finf out the real outcome of very functions or module writen
