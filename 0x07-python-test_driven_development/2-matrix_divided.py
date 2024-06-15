32fe5f189bbacbea2bcdbba4c1020b85a9610819
ddanimoh <ddanimoh@gmail.com>
2024-06-15 21:26:48 +0100

    test_update

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Module for function that receive list of lists, or other data
+Or other python data structure (set tuple)
+Each data must be interger or float type
+And len of each nested most be same.
+"""
+def matrix_divided(matrix, div):
